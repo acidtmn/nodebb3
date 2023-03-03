27569ae55c59285ab143642b24225dbf52825b57
acidtmn <acidtmn@yandex.ru>
2023-03-03 12:32:32 +0500

    Update testdeploy.sh

--- scripts/testdeploy.sh
+++ scripts/testdeploy.sh
@@ -2,5 +2,5 @@
 docker stop myNodeBB
 docker rm myNodeBB
 yes | docker volume prune
-docker create --name myNodeBB --init --restart always -p 4567:4567 -v nodebb-data:/var/lib/redis -v nodebb-files:/opt/nodebb/public/uploads -v nodebb-config:/etc/nodebb nilsramsperger/nodebb
+docker create --name myNodeBB --init --restart always -p 4567:4567 -v nodebb-data:/var/lib/redis -v nodebb-files:/opt/nodebb/public/uploads -v nodebb-config:/etc/nodebb acidtmn/nodebb3
 docker start myNodeBB
